292a2d29bebd35d2537de20172ecdc0c51da241b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-09-14 18:59:45 +0400

    Add install

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget https://russianaicup.ru/s/1565045205765/assets/local-runner/codeball2018-linux.tar.gz
+tar -xzf codeball2018-linux.tar.gz
+wget https://russianaicup.ru/s/1565045205765/assets/cgdks/python.3-cgdk.zip
